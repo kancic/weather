c7a1044e6fde9654477025f5554cc4614018d10d
Karim Ancic <karim.ancic@shape404.agency>
2022-09-03 17:58:04 +0200

    Project init

---
+++ app/src/main/java/ancic/karim/weatherancic/MainActivity.kt
@@ -0,0 +1,33 @@
+package ancic.karim.weatherancic
+
+import ancic.karim.weatherancic.databinding.ActivityMainBinding
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/ancic/karim/weatherancic/feature/weather/WeatherFragment.kt
@@ -0,0 +1,7 @@
+package ancic.karim.weatherancic.feature.weather
+
+import ancic.karim.weatherancic.R
+import androidx.fragment.app.Fragment
+
+class WeatherFragment : Fragment(R.layout.fragment_weather) {
+}
