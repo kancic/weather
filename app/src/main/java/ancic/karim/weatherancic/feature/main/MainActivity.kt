dd2b97f41200ed07b9d8f668cf74fb1a3af5344e
Karim Ancic <karim.ancic@shape404.agency>
2022-09-07 00:29:38 +0200

    Implemented city search and select

--- app/src/main/java/ancic/karim/weatherancic/feature/main/MainActivity.kt
+++ app/src/main/java/ancic/karim/weatherancic/feature/main/MainActivity.kt
@@ -4,10 +4,16 @@ import ancic.karim.weatherancic.R
 import ancic.karim.weatherancic.databinding.ActivityMainBinding
 import ancic.karim.weatherancic.extensions.nonNull
 import ancic.karim.weatherancic.utils.LocationUtil
+import android.app.SearchManager
+import android.database.MatrixCursor
 import android.os.Bundle
+import android.provider.BaseColumns
+import android.widget.CursorAdapter
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.WindowCompat
+import androidx.cursoradapter.widget.SimpleCursorAdapter
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -42,12 +48,70 @@ class MainActivity : AppCompatActivity() {
             viewModel.location.value = it
         }
 
+        setupSearchCityInput()
+
         observeCity()
+        observeSuggestionCities()
+    }
+
+    private fun setupSearchCityInput() {
+        binding.searchCity.apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    viewModel.searchCity(query.trim())
+                    return true
+                }
+
+                override fun onQueryTextChange(query: String): Boolean {
+                    viewModel.searchCity(query.trim())
+                    return true
+                }
+
+            })
+            setOnSuggestionListener(object : SearchView.OnSuggestionListener {
+                override fun onSuggestionSelect(position: Int): Boolean {
+                    viewModel.selectSuggestion(position)
+                    return true
+                }
+
+                override fun onSuggestionClick(position: Int): Boolean {
+                    viewModel.selectSuggestion(position)
+                    return true
+                }
+
+            })
+        }
     }
 
     private fun observeCity() {
         viewModel.city.nonNull().observe(this) {
             binding.searchCity.setQuery(it, false)
+        }
+    }
+
+    private fun observeSuggestionCities() {
+        viewModel.citySuggestions.observe(this) {
+            setCitySuggestions(it)
+        }
+    }
+
+    private fun setCitySuggestions(suggestions: List<String>) {
+        val from = arrayOf(SearchManager.SUGGEST_COLUMN_TEXT_1)
+        val to = intArrayOf(android.R.id.text1)
+        val cursorAdapter = SimpleCursorAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            null,
+            from,
+            to,
+            CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+        )
+        binding.searchCity.suggestionsAdapter = cursorAdapter
+
+        val cursor = MatrixCursor(arrayOf(BaseColumns._ID, SearchManager.SUGGEST_COLUMN_TEXT_1))
+        suggestions.forEachIndexed { index, suggestion ->
+            cursor.addRow(arrayOf(index, suggestion))
+            cursorAdapter.changeCursor(cursor)
         }
     }
 }
--- app/src/main/java/ancic/karim/weatherancic/feature/main/MainViewModel.kt
+++ app/src/main/java/ancic/karim/weatherancic/feature/main/MainViewModel.kt
@@ -1,18 +1,25 @@
 package ancic.karim.weatherancic.feature.main
 
 import android.app.Application
+import android.location.Address
 import android.location.Geocoder
 import android.location.Location
+import android.location.LocationManager
 import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(application: Application, repository: MainRepository) : ViewModel() {
+    private val geocoder = Geocoder(application)
+    private val addressSuggestions = MutableLiveData<List<Address>>()
+
     val location = MutableLiveData<Location?>()
     val city = MutableLiveData<String?>()
+    val citySuggestions = addressSuggestions.map { it.map { it.locality }.filterNotNull() }
     val forecast = location.asFlow().filterNotNull().distinctUntilChanged().flatMapConcat {
         repository.getForecast(it)
     }.asLiveData()
@@ -23,5 +30,24 @@ class MainViewModel @Inject constructor(application: Application, repository: Ma
         }.map { it.firstOrNull() }.flowOn(Dispatchers.IO).filterNotNull().onEach {
             city.value = it.locality
         }.launchIn(viewModelScope)
+    }
+
+    fun searchCity(city: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            addressSuggestions.postValue(if (city.isNotEmpty()) {
+                geocoder.getFromLocationName(city, 4).filter { it.locality?.contains(city, ignoreCase = true) == true }
+            } else {
+                emptyList()
+            })
+        }
+    }
+
+    fun selectSuggestion(position: Int) {
+        location.value = addressSuggestions.value?.get(position)?.let { address ->
+            Location(LocationManager.PASSIVE_PROVIDER).apply {
+                latitude = address.latitude
+                longitude = address.longitude
+            }
+        }
     }
 }
